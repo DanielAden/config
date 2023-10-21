9e40bba5cd59d1d0fce420ffe78817de925af918
Daniel Aden <Daniel.Aden@trustage.com>
2023-10-21 10:27:30 -0700

    tresitter context plugin + session keybindings

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -13,11 +13,13 @@
 pcall(require, "user.work-config")
 pcall(require, "user.personal-config")
 
+local SESSION_DIR = "~/.vimsessions"
+
 lvim.colorscheme = "carbonfox"
 vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = function()
-		vim.cmd([[hi MatchParen cterm=bold gui=bold guifg=none guibg=red]])
-	end,
+  callback = function()
+    vim.cmd([[hi MatchParen cterm=bold gui=bold guifg=none guibg=red]])
+  end,
 })
 
 -- vim options
@@ -30,9 +32,9 @@ vim.opt.autoread = true
 
 lvim.log.level = "info"
 lvim.format_on_save = {
-	enabled = true,
-	pattern = "*.lua,*.ts,*.tsx,*.go",
-	timeout = 2000,
+  enabled = true,
+  pattern = "*.lua,*.ts,*.tsx,*.go",
+  timeout = 2000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -72,119 +74,119 @@ lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 local function organize_imports()
-	local params = {
-		command = "_typescript.organizeImports",
-		arguments = { vim.api.nvim_buf_get_name(0) },
-		title = "",
-	}
-	vim.lsp.buf.execute_command(params)
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = "",
+  }
+  vim.lsp.buf.execute_command(params)
 end
 
 require("lspconfig").tsserver.setup({
-	commands = {
-		OrganizeImports = {
-			organize_imports,
-			description = "Organize Imports",
-		},
-	},
-	init_options = {
-		preferences = {
-			importModuleSpecifierPreference = "relative",
-			importModuleSpecifierEnding = "minimal",
-		},
-	},
+  commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports",
+    },
+  },
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = "relative",
+      importModuleSpecifierEnding = "minimal",
+    },
+  },
 })
 
 require("lspconfig").gopls.setup({
-	settings = {
-		gopls = {
-			usePlaceholders = true,
-			analyses = {
-				unusedparams = true,
-			},
-		},
-	},
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
 })
 
 -- -- Disable lvim custom finder for <leader>f
 lvim.builtin.which_key.mappings["f"] = {
-	require("lvim.core.telescope.custom-finders").find_project_files,
-	"Find File",
+  require("lvim.core.telescope.custom-finders").find_project_files,
+  "Find File",
 }
 
 -- Reloads buffer
 lvim.builtin.which_key.mappings["r"] = {
-	":checktime<CR>",
-	"reload buffer",
+  ":checktime<CR>",
+  "reload buffer",
 }
 
 lvim.builtin.which_key.mappings["x"] = {
-	name = "Close",
-	b = { ":bd<CR>", "Buffer" },
-	w = { ":close<CR>", "Window" },
-	a = { ":qa<CR>", "all" },
+  name = "Close",
+  b = { ":bd<CR>", "Buffer" },
+  w = { ":close<CR>", "Window" },
+  a = { ":qa<CR>", "all" },
 }
 
 lvim.builtin.which_key.mappings["t"] = {
-	name = [[Diagnostics/Tabs]],
-	t = { "<cmd>TroubleToggle<cr>", "trouble" },
-	w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
-	d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
-	q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-	l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-	r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
-	n = { "<cmd>tabnext<cr>", "tab next" },
-	p = { "<cmd>tabprev<cr>", "tab prev" },
-	a = { "<cmd>tabnew<cr>", "add tab" },
+  name = [[Diagnostics/Tabs]],
+  t = { "<cmd>TroubleToggle<cr>", "trouble" },
+  w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+  d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+  r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+  n = { "<cmd>tabnext<cr>", "tab next" },
+  p = { "<cmd>tabprev<cr>", "tab prev" },
+  a = { "<cmd>tabnew<cr>", "add tab" },
 }
 
 lvim.builtin.which_key.mappings["b"]["f"] = { "<cmd>Telescope scope buffers<cr>", "Find Scoped Buffer" }
 
 lvim.builtin.which_key.mappings["j"] = {
-	name = "Terminal",
-	j = { "<cmd>ToggleTerm<cr>", "Open Terminal" },
+  name = "Terminal",
+  j = { "<cmd>ToggleTerm<cr>", "Open Terminal" },
 }
 
 local builtin = require("telescope.builtin")
 lvim.builtin.which_key.mappings["s"] = vim.tbl_extend("keep", lvim.builtin.which_key.mappings["s"], {
-	T = {
-		builtin.grep_string,
-		"search word under cursor",
-	},
-	i = {
-		function()
-			builtin.live_grep({
-				prompt_title = "Find Text (Ignoring Test Files)",
-				glob_pattern = [[!**/{acceptance,__test__}/*]],
-			})
-		end,
-		"Find Text (Ignore Test Files)",
-	},
-	x = {
-		builtin.treesitter,
-		"Search Code (Treesitter)",
-	},
-	o = {
-		builtin.current_buffer_fuzzy_find,
-		"Search Open Buffer",
-	},
+  T = {
+    builtin.grep_string,
+    "search word under cursor",
+  },
+  i = {
+    function()
+      builtin.live_grep({
+        prompt_title = "Find Text (Ignoring Test Files)",
+        glob_pattern = [[!**/{acceptance,__test__}/*]],
+      })
+    end,
+    "Find Text (Ignore Test Files)",
+  },
+  x = {
+    builtin.treesitter,
+    "Search Code (Treesitter)",
+  },
+  o = {
+    builtin.current_buffer_fuzzy_find,
+    "Search Open Buffer",
+  },
 })
 
 lvim.builtin.which_key.mappings["b"] = vim.tbl_extend("keep", lvim.builtin.which_key.mappings["b"], {
-	o = { "<cmd>OrganizeImports<cr>", "Organize Imports" },
+  o = { "<cmd>OrganizeImports<cr>", "Organize Imports" },
 })
 
 -- vim-test
 vim.api.nvim_create_user_command("ClearTestBuffers", function()
-	local buffers = vim.api.nvim_list_bufs()
-	for index, value in ipairs(buffers) do
-		local name = vim.api.nvim_buf_get_name(value)
-		local isTestBuffer = string.find(name, "term://") and string.find(name, "react%-scripts")
-			or string.find(name, "jest")
-		if isTestBuffer then
-			vim.cmd(string.format("bw! %s", value))
-		end
-	end
+  local buffers = vim.api.nvim_list_bufs()
+  for index, value in ipairs(buffers) do
+    local name = vim.api.nvim_buf_get_name(value)
+    local isTestBuffer = string.find(name, "term://") and string.find(name, "react%-scripts")
+        or string.find(name, "jest")
+    if isTestBuffer then
+      vim.cmd(string.format("bw! %s", value))
+    end
+  end
 end, { nargs = 0 })
 
 vim.g["test#strategy"] = "neovim"
@@ -195,46 +197,46 @@ vim.g["test#javascript#reactscripts#options"] = "--watchAll=false --env=jsdom"
 vim.g["test#neovim#start_normal"] = 1
 -- vim.g["test#enabled_runners"] = ["javascript#jest"];
 lvim.builtin.which_key.mappings["u"] = {
-	name = "Unit Tests",
-	c = {
-		function()
-			vim.cmd("ClearTestBuffers") -- Wipes any previous test buffers
-		end,
-		"Clear Test Buffers",
-	},
-	t = {
-		function()
-			vim.cmd("ClearTestBuffers") -- Wipes any previous test buffers
-			vim.cmd("TestNearest")
-			vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } }) -- puts cursor in previous position
-		end,
-		"Test Nearest",
-	},
-	T = {
-		function()
-			vim.cmd("ClearTestBuffers")
-			vim.cmd("TestFile")
-			vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } })
-		end,
-		"Test File",
-	},
-	a = {
-		function()
-			vim.cmd("ClearTestBuffers")
-			vim.cmd("TestSuite")
-			vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } })
-		end,
-		"Test Suite",
-	},
-	l = {
-		function()
-			vim.cmd("ClearTestBuffers")
-			vim.cmd("TestLast")
-			vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } })
-		end,
-		"Run Last Test",
-	},
-	g = { "<cmd>TestVisit<cr>", "Navigates to last visited test file" },
+  name = "Unit Tests",
+  c = {
+    function()
+      vim.cmd("ClearTestBuffers") -- Wipes any previous test buffers
+    end,
+    "Clear Test Buffers",
+  },
+  t = {
+    function()
+      vim.cmd("ClearTestBuffers")                           -- Wipes any previous test buffers
+      vim.cmd("TestNearest")
+      vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } }) -- puts cursor in previous position
+    end,
+    "Test Nearest",
+  },
+  T = {
+    function()
+      vim.cmd("ClearTestBuffers")
+      vim.cmd("TestFile")
+      vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } })
+    end,
+    "Test File",
+  },
+  a = {
+    function()
+      vim.cmd("ClearTestBuffers")
+      vim.cmd("TestSuite")
+      vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } })
+    end,
+    "Test Suite",
+  },
+  l = {
+    function()
+      vim.cmd("ClearTestBuffers")
+      vim.cmd("TestLast")
+      vim.cmd.exe({ args = { '"normal \\<C-w>\\<C-p>"' } })
+    end,
+    "Run Last Test",
+  },
+  g = { "<cmd>TestVisit<cr>", "Navigates to last visited test file" },
 }
 
 -- function! VagrantTransform(cmd) abort
@@ -282,20 +284,20 @@ vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pylyzer" })
 --
 local actions = require("telescope.actions")
 lvim.builtin.telescope.defaults = {
-	wrap_results = true,
-	layout_strategy = "horizontal",
-	layout_config = {
-		prompt_position = "top",
-		width = 0.95,
-		height = 0.90,
-		preview_width = 0.5,
-	},
-	sorting_strategy = "ascending",
-	mappings = {
-		i = {
-			["<esc>"] = actions.close,
-		},
-	},
+  wrap_results = true,
+  layout_strategy = "horizontal",
+  layout_config = {
+    prompt_position = "top",
+    width = 0.95,
+    height = 0.90,
+    preview_width = 0.5,
+  },
+  sorting_strategy = "ascending",
+  mappings = {
+    i = {
+      ["<esc>"] = actions.close,
+    },
+  },
 }
 
 vim.lsp.handlers["textDocument/references"] = require("telescope/builtin").lsp_references
@@ -303,86 +305,87 @@ vim.lsp.handlers["textDocument/references"] = require("telescope/builtin").lsp_r
 -- linters, formatters and code actions <https://www.lunarvim.org/docs/languages#lintingformatting>
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
-	{ command = "stylua" },
-	-- {
-	--   command = "prettier",
-	--   -- extra_args = { "--print-width", "100" },
-	--   filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-	-- },
-	-- {
-	--   command = "eslint_d"
-	-- },
-	{
-		command = "prettierd",
-	},
-	-- {
-	--   { exe = "jq", args = { "--indent", "2" }, filetypes = { "json" } },
-	-- }
-	{
-		command = "gofumpt",
-	},
-	{
-		command = "golines",
-	},
+  { command = "stylua" },
+  -- {
+  --   command = "prettier",
+  --   -- extra_args = { "--print-width", "100" },
+  --   filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  -- },
+  -- {
+  --   command = "eslint_d"
+  -- },
+  {
+    command = "prettierd",
+  },
+  -- {
+  --   { exe = "jq", args = { "--indent", "2" }, filetypes = { "json" } },
+  -- }
+  {
+    command = "gofumpt",
+  },
+  {
+    command = "golines",
+  },
 })
 
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-	-- { command = "flake8", filetypes = { "python" } },
-	-- {
-	--   command = "shellcheck",
-	--   args = { "--severity", "warning" },
-	-- },
-	{
-		command = "eslint_d",
-	},
-	{ command = "golangci-lint" },
+  -- { command = "flake8", filetypes = { "python" } },
+  -- {
+  --   command = "shellcheck",
+  --   args = { "--severity", "warning" },
+  -- },
+  {
+    command = "eslint_d",
+  },
+  { command = "golangci-lint" },
 })
 
 local code_actions = require("lvim.lsp.null-ls.code_actions")
 code_actions.setup({
-	{
-		exe = "eslint_d",
-		filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-	},
+  {
+    exe = "eslint_d",
+    filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+  },
 })
 
 -- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-	{
-		"vim-test/vim-test",
-	},
-	{
-		"folke/trouble.nvim",
-		cmd = "TroubleToggle",
-	},
-	{ "tpope/vim-repeat" },
-	{
-		"tpope/vim-surround",
+  {
+    "vim-test/vim-test",
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  { "tpope/vim-repeat" },
+  {
+    "tpope/vim-surround",
 
-		-- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
-		-- setup = function()
-		--  vim.o.timeoutlen = 500
-		-- end
-	},
-	{
-		"ggandor/leap.nvim",
-		name = "leap",
-		config = function()
-			require("leap").add_default_mappings()
-		end,
-	},
-	{
-		"mxsdev/nvim-dap-vscode-js",
-	},
-	{
-		"ray-x/lsp_signature.nvim",
-	},
-	{ "nvim-treesitter/playground" },
-	{ "norcalli/nvim-colorizer.lua" },
-	{ "EdenEast/nightfox.nvim" },
-	{ "tiagovla/scope.nvim" },
-	{ "windwp/nvim-ts-autotag" },
+    -- make sure to change the value of `timeoutlen` if it's not triggering correctly, see https://github.com/tpope/vim-surround/issues/117
+    -- setup = function()
+    --  vim.o.timeoutlen = 500
+    -- end
+  },
+  {
+    "ggandor/leap.nvim",
+    name = "leap",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+  {
+    "mxsdev/nvim-dap-vscode-js",
+  },
+  {
+    "ray-x/lsp_signature.nvim",
+  },
+  { "nvim-treesitter/playground" },
+  { "norcalli/nvim-colorizer.lua" },
+  { "EdenEast/nightfox.nvim" },
+  { "tiagovla/scope.nvim" },
+  { "windwp/nvim-ts-autotag" },
+  { "nvim-treesitter/nvim-treesitter-context" }
 }
 
 -- -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
@@ -410,35 +413,35 @@ lvim.plugins = {
 local masonPackages = vim.fn.stdpath("data") .. "/mason/packages/"
 local dap = require("dap")
 dap.adapters.node2 = {
-	type = "executable",
-	command = "node",
-	args = { masonPackages .. "node-debug2-adapter/out/src/nodeDebug.js" },
+  type = "executable",
+  command = "node",
+  args = { masonPackages .. "node-debug2-adapter/out/src/nodeDebug.js" },
 }
 
 for _, language in ipairs({ "typescript", "javascript" }) do
-	require("dap").configurations[language] = {
-		{
-			name = "Attach",
-			type = "node2",
-			request = "attach",
-			processId = require("dap.utils").pick_process,
-			cwd = "${workspaceFolder}",
-		},
-		{
-			name = "Launch App.ts",
-			type = "node2",
-			request = "launch",
-			cwd = "${workspaceFolder}",
-			args = { "-r", "ts-node/register/transpile-only", "./src/App.ts" },
-		},
-		{
-			name = "Launch File",
-			type = "node2",
-			request = "launch",
-			program = "${file}",
-			cwd = "${workspaceFolder}",
-		},
-	}
+  require("dap").configurations[language] = {
+    {
+      name = "Attach",
+      type = "node2",
+      request = "attach",
+      processId = require("dap.utils").pick_process,
+      cwd = "${workspaceFolder}",
+    },
+    {
+      name = "Launch App.ts",
+      type = "node2",
+      request = "launch",
+      cwd = "${workspaceFolder}",
+      args = { "-r", "ts-node/register/transpile-only", "./src/App.ts" },
+    },
+    {
+      name = "Launch File",
+      type = "node2",
+      request = "launch",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+  }
 end
 
 local cfg = {} -- add your config here
@@ -447,71 +450,71 @@ require("lsp_signature").setup(cfg)
 -- Markdown Settings
 vim.g.markdown_folding = true
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "markdown",
-	callback = function()
-		vim.opt.wrap = true
-		vim.opt.linebreak = true
-	end,
+  pattern = "markdown",
+  callback = function()
+    vim.opt.wrap = true
+    vim.opt.linebreak = true
+  end,
 })
 
 require("colorizer").setup()
 local trim = function(s)
-	return string.gsub(s, "[\r\n]", "")
+  return string.gsub(s, "[\r\n]", "")
 end
 
 local split = function(str, pat)
-	local t = {} -- NOTE: use {n = 0} in Lua-5.0
-	local fpat = "(.-)" .. pat
-	local last_end = 1
-	local s, e, cap = str:find(fpat, 1)
-	while s do
-		if s ~= 1 or cap ~= "" then
-			table.insert(t, cap)
-		end
-		last_end = e + 1
-		s, e, cap = str:find(fpat, last_end)
-	end
-	if last_end <= #str then
-		cap = str:sub(last_end)
-		table.insert(t, cap)
-	end
-	return t
+  local t = {} -- NOTE: use {n = 0} in Lua-5.0
+  local fpat = "(.-)" .. pat
+  local last_end = 1
+  local s, e, cap = str:find(fpat, 1)
+  while s do
+    if s ~= 1 or cap ~= "" then
+      table.insert(t, cap)
+    end
+    last_end = e + 1
+    s, e, cap = str:find(fpat, last_end)
+  end
+  if last_end <= #str then
+    cap = str:sub(last_end)
+    table.insert(t, cap)
+  end
+  return t
 end
 
 local openFileInADO = function(branch)
-	local currentBranch = branch or trim(vim.fn.system({ "git", "rev-parse", "--abbrev-ref", "HEAD" }))
-	local fileName = "/" .. vim.fn.expand("%:~:.")
-	local remoteOrigin = trim(vim.fn.system({ "git", "config", "--get", "remote.origin.url" }))
-	local remoteUrl = split(remoteOrigin, "@")[2]
+  local currentBranch = branch or trim(vim.fn.system({ "git", "rev-parse", "--abbrev-ref", "HEAD" }))
+  local fileName = "/" .. vim.fn.expand("%:~:.")
+  local remoteOrigin = trim(vim.fn.system({ "git", "config", "--get", "remote.origin.url" }))
+  local remoteUrl = split(remoteOrigin, "@")[2]
 
-	vim.print(remoteUrl)
-	local cmd = string.format(
-		[[open -a "/Applications/Google Chrome.app" 'https://%s?path=%s&version=GB%s&_a=contents']],
-		remoteUrl,
-		fileName,
-		currentBranch
-	)
-	os.execute(cmd)
+  vim.print(remoteUrl)
+  local cmd = string.format(
+    [[open -a "/Applications/Google Chrome.app" 'https://%s?path=%s&version=GB%s&_a=contents']],
+    remoteUrl,
+    fileName,
+    currentBranch
+  )
+  os.execute(cmd)
 end
 
 lvim.builtin.which_key.mappings["a"] = {
-	name = "ADO",
-	b = {
-		openFileInADO,
-		"Open File and Branch in ADO",
-	},
-	f = {
-		function()
-			openFileInADO("feature-int")
-		end,
-		"Open File and Branch in ADO",
-	},
-	d = {
-		function()
-			openFileInADO("develop")
-		end,
-		"Open File on Develop in ADO",
-	},
+  name = "ADO",
+  b = {
+    openFileInADO,
+    "Open File and Branch in ADO",
+  },
+  f = {
+    function()
+      openFileInADO("feature-int")
+    end,
+    "Open File and Branch in ADO",
+  },
+  d = {
+    function()
+      openFileInADO("develop")
+    end,
+    "Open File on Develop in ADO",
+  },
 }
 
 -- git blame -L 37,37 -l -- src/App.ts
@@ -528,3 +531,31 @@ require("nvim-ts-autotag").setup()
 -- })
 --
 lvim.builtin.nvimtree.setup.view.adaptive_size = true
+
+vim.api.nvim_create_user_command("DirectorySessionSave", function()
+  local currentDirectory = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  local path = SESSION_DIR .. "/" .. currentDirectory .. ".vim"
+  vim.cmd("mks! " .. path);
+  vim.print("Saved " .. path)
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("DirectorySessionLoad", function()
+  local currentDirectory = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  local path = SESSION_DIR .. "/" .. currentDirectory .. ".vim"
+  vim.cmd("source " .. path);
+  vim.print("Loaded " .. path)
+end, { nargs = 0 })
+
+lvim.builtin.which_key.mappings["S"] = {
+  name = "Sessions",
+  s = { ":DirectorySessionSave<CR>", "Make/Save Directory Session" },
+  l = { ":DirectorySessionLoad<CR>", "Load Session For Current Dir" },
+}
+
+-- nvim-treesitter-context config
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.cmd("TSContextEnable")
+  end,
+})
+lvim.builtin.which_key.mappings["T"]["c"] = { ":TSContextEnable<CR>", "Treesitter Context Enable" }
