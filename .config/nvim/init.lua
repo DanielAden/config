e8ebafe94b710d124beedaa1549c9b54e526cf29
Daniel Aden <Daniel.Aden@trustage.com>
2024-05-19 15:35:25 -0700

    nvim: added initial vscode nvim extension config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,38 +4,45 @@ vim.cmd([[
   source ~/.vimrc
 ]])
 
+if vim.g.vscode then
+  require("vscode_setup")
+else
+  vim.g.auto_format_enabled = true
+  vim.g.inlay_hints_enabled = true
+
+  -- require('firenvim')
+
+  require 'lazy-bootstrap'
+
+  require("lazy").setup("plugins")
+
+  require 'options'
+
+  require 'keymaps'
+
+  require 'nvim-tree-config'
+
+  require 'which-key-setup'
+
+  require 'bufferline-config'
+
+  require 'telescope-setup'
+
+  require 'treesitter-setup'
+
+  require 'vim-test-setup'
+
+  require 'cmp-setup'
+
+  require 'lsp-setup'
+
+  require 'null_ls'
+
+  require 'scratch'
+end
+
+
+
 -- TODO LIST
 -- * look into retaining file history after close
-
-vim.g.auto_format_enabled = true
-vim.g.inlay_hints_enabled = true
-
--- require('firenvim')
-
-require 'lazy-bootstrap'
-
-require("lazy").setup("plugins")
-
-require 'options'
-
-require 'keymaps'
-
-require 'nvim-tree-config'
-
-require 'which-key-setup'
-
-require 'bufferline-config'
-
-require 'telescope-setup'
-
-require 'treesitter-setup'
-
-require 'vim-test-setup'
-
-require 'cmp-setup'
-
-require 'lsp-setup'
-
-require 'null_ls'
-
-require 'scratch'
+--
---
+++ .config/nvim/lua/vscode_setup.lua
@@ -0,0 +1,16 @@
+-- Settings for when neovim is running inside of vscode 
+
+
+vim.keymap.set('n', 'H', "<Cmd>lua require('vscode-neovim').call('workbench.action.previousEditor')<CR>", { desc = ''})
+vim.keymap.set('n', 'L', "<Cmd>lua require('vscode-neovim').call('workbench.action.nextEditor')<CR>", { desc = ''})
+
+-- <Cmd>lua require('vscode-neovim').call('editor.action.formatSelection')<CR>
+vim.keymap.set('n', '<leader>w', "<Cmd>lua require('vscode-neovim').call('workbench.action.files.save')<CR>", { desc = ''})
+
+
+vim.keymap.set('n', 'rw', "<Cmd>lua require('vscode-neovim').call('databricks.run.runEditorContentsAsWorkflow')<CR>", { desc = ''})
+vim.keymap.set('n', 'rf', "<Cmd>lua require('vscode-neovim').call('databricks.run.runEditorContents')<CR>", { desc = ''})
+
+
+vim.keymap.set('n', '<leader>e', "<Cmd>lua require('vscode-neovim').call('workbench.action.toggleSidebarVisibility')<CR>", { desc = ''})
+vim.keymap.set('n', '<leader>gc', "<Cmd>lua require('vscode-neovim').call('workbench.panel.chat.view.copilot.focus')<CR>", { desc = ''})
