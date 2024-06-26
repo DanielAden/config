b4164fbcd4b947dfc9d38fd500a7376851f9d3b1
Daniel Aden <Daniel.Aden@trustage.com>
2024-06-25 18:29:32 -0700

    nvim: add coplit chat visual mode keybinds

--- .config/nvim/lua/copilot-setup.lua
+++ .config/nvim/lua/copilot-setup.lua
@@ -6,13 +6,11 @@
 -- })
 -- vim.g.copilot_no_tab_map = true
 
- 
-vim.keymap.set('n', '<leader><leader>cc', ':CopilotChatToggle<CR>', { desc = '[C]opilot [C]hat Toggle' })
--- vim.keymap.set('n', '<leader><leader>co', ':CopilotChatOpen<CR>', { desc = '[C]opilot [C]hat Toggle' })
-vim.keymap.set('n', '<leader><leader>cx', ':CopilotChatClose<CR>', { desc = '[C]opilot [C]hat Toggle' })
-vim.keymap.set('n', '<leader><leader>cr', ':CopilotChatReset<CR>', { desc = '[C]opilot Chat [R]eset' })
+vim.keymap.set({ "n", "v" }, "<leader><leader>cc", ":CopilotChatToggle<CR>", { desc = "[C]opilot [C]hat Toggle" })
+vim.keymap.set({ "n", "v" }, "<leader><leader>cx", ":CopilotChatClose<CR>", { desc = "[C]opilot [C]hat Toggle" })
+vim.keymap.set({ "n", "v" }, "<leader><leader>cr", ":CopilotChatReset<CR>", { desc = "[C]opilot Chat [R]eset" })
 
-vim.keymap.set('n', '<leader><leader>ct', ':CopilotChatTests<CR>', { desc = '[C]opilot Chat [T]ests' })
-vim.keymap.set('n', '<leader><leader>cf', ':CopilotChatFix<CR>', { desc = '[C]opilot Chat [F]ix' })
-vim.keymap.set('n', '<leader><leader>cd', ':CopilotChatDocs<CR>', { desc = '[C]opilot Chat [D]ocs' })
-vim.keymap.set('n', '<leader><leader>co', ':CopilotChatOptimize<CR>', { desc = '[C]opilot Chat [O]ptimize' })
+vim.keymap.set({ "n", "v" }, "<leader><leader>ct", ":CopilotChatTests<CR>", { desc = "[C]opilot Chat [T]ests" })
+vim.keymap.set({ "n", "v" }, "<leader><leader>cf", ":CopilotChatFix<CR>", { desc = "[C]opilot Chat [F]ix" })
+vim.keymap.set({ "n", "v" }, "<leader><leader>cd", ":CopilotChatDocs<CR>", { desc = "[C]opilot Chat [D]ocs" })
+vim.keymap.set({ "n", "v" }, "<leader><leader>co", ":CopilotChatOptimize<CR>", { desc = "[C]opilot Chat [O]ptimize" })
