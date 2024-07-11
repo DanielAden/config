10d02609168f250be8c2e3cd65a0b6ed6c68f4a8
DanielAden <danaden333@yahoo.com>
2024-07-10 20:04:50 -0700

    nvim: markdown after file

---
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,12 @@
+-- Markdown Settings
+vim.g.markdown_folding = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+-- vim.api.nvim_create_autocmd("FileType", {
+-- 	pattern = "markdown",
+-- 	callback = function()
+-- 		vim.opt.wrap = true
+-- 		vim.opt.linebreak = true
+-- 	end,
+-- })
