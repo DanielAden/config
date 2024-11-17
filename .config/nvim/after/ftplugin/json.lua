71199fa3f4fddcfe09c8b91d4a1f7230426428b5
Daniel Aden <Daniel.Aden@trustage.com>
2024-11-17 10:43:07 -0800

    nvim: add json minify cmd using jq

--- .config/nvim/after/ftplugin/json.lua
+++ .config/nvim/after/ftplugin/json.lua
@@ -2,7 +2,8 @@
 -- set foldexpr=nvim_treesitter#foldexpr()
 -- set foldlevel=99
 
-
 vim.opt.foldmethod = "indent"
 -- Defaults folds to be open
-vim.opt.foldlevel=99 
+vim.opt.foldlevel = 99
+
+vim.keymap.set("n", "<leader>m", "<Cmd>%!jq -c . <CR>", { desc = "Minify JSON" })
